28bd777fc1d801611f2f2b50b5d64fcc9578968f
carlosxhcp <carlosxhcp@gmail.com>
2026-05-14 09:54:23 -0300

    fix urls3

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", include("pages.urls")),
     path("", include(("products.urls", "products"), namespace="products")),
     path("", include("cart.urls")),
+    
 ]
 
 if settings.DEBUG:
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = "products"
+
 urlpatterns = [
+    path(
+        "produto/<slug:slug>/",
+        views.product_detail,
+        name="product_detail"
+    ),
+
     path(
         "produto/<slug:slug>/checkout/",
         views.checkout_stripe_product,
