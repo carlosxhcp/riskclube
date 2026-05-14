88a258beb52bb4904686e02e6ae07ca94f52591e
carlosxhcp <carlosxhcp@gmail.com>
2026-05-14 08:43:40 -0300

    fix errors

--- core/urls.py
+++ core/urls.py
@@ -2,18 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("", include("products.urls")),
     path("", include("cart.urls")),
-    path(
-    "produto/<slug:slug>/checkout/",
-    views.checkout_stripe_product,
-    name="checkout_stripe_product"
-),
 ]
 
 if settings.DEBUG:
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = "products"
-
 urlpatterns = [
-    path("produto/<slug:slug>/", views.product_detail, name="product_detail"),
+    path(
+        "produto/<slug:slug>/checkout/",
+        views.checkout_stripe_product,
+        name="checkout_stripe_product"
+    ),
 ]
