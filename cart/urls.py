b759b1ac977735cbde2321c4835eefed78c6f7bc
carlosxhcp <carlosxhcp@gmail.com>
2026-05-14 11:22:48 -0300

    re

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path("infinitepay/checkout/", views.checkout_infinitepay, name="checkout_infinitepay"),
-    path("infinitepay/retorno/", views.infinitepay_return, name="infinitepay_return"),
-    path("infinitepay/webhook/", views.infinitepay_webhook, name="infinitepay_webhook"),
 ]
