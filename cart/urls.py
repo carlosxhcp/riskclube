81b10429ae9f57bab2e51c6631881db4ee4e47c2
carlosxhcp <carlosxhcp@gmail.com>
2026-05-15 07:44:23 -0300

    update cart + add

--- cart/urls.py
+++ cart/urls.py
@@ -1,31 +1,22 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path(
-        "infinitepay/checkout/<int:product_id>/",
-        views.checkout_infinitepay,
-        name="checkout_infinitepay"
-    ),
-    path(
-        "add/",
-        views.cart_add_ajax,
-        name="cart_add_ajax"
-    ),
-
-    path(
-        "data/",
-        views.cart_data,
-        name="cart_data"
-    ),
-]
-
-from django.urls import path
-from . import views
+app_name = "cart"
 
 urlpatterns = [
     path("add/", views.cart_add_ajax, name="cart_add_ajax"),
     path("data/", views.cart_data, name="cart_data"),
     path("update/", views.cart_update, name="cart_update"),
-    path("infinitepay/checkout/", views.checkout_infinitepay_cart, name="checkout_infinitepay_cart"),
+
+    path(
+        "infinitepay/checkout/<int:product_id>/",
+        views.checkout_infinitepay,
+        name="checkout_infinitepay"
+    ),
+
+    path(
+        "infinitepay/checkout/",
+        views.checkout_infinitepay_cart,
+        name="checkout_infinitepay_cart"
+    ),
 ]
